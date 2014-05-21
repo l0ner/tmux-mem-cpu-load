c4753e5a1c34f8dfe55a88ca26c2e3dad4a8e28a
l0ner <pwslts@gmail.com>
2014-05-21 19:53:35 +0000

    dunno

--- spark.cc
+++ spark.cc
@@ -4,19 +4,32 @@
 #include "spark.h"
 
 char getSparkByPrecentage(unsigned value) {
-   char ticks = " ▁▂▃▄▅▆▇█";
+   char ticks = "▁▂▃▄▅▆▇█";
 
-   unsigned tickVal = (float(value) / 9);
+   unsigned tickVal = (float(value) / 8);
 
    return ticks[tickVal];
 }
 
 
 char getSparkByValue(unsigned value, unsigned max) {
-   char ticks = " ▁▂▃▄▅▆▇█";
+   char ticks = "▁▂▃▄▅▆▇█";
 
-   unsigned tickVal = (float(value) / float(max) * 9);
+   unsigned tickVal = (float(value) / float(max) * 8);
 
    return ticks[tickVal];
 }
 
+/*
+ * TODO
+ * This function should accept an array of values,
+ * and output length, find max and min in the array 
+ * and output n-sized spark line
+ * in this case template-based input arg may be needed
+ * This: http://rosettacode.org/wiki/Sparkline_in_unicode#C.2B.2B
+ * approach might work too, but it's more complicated
+char getSparkFromValues(){
+
+}
+
+*/
